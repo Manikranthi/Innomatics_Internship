598b9cce21a5d195a70eb33d35d6cd907ce21ee1
Manikranthi <77731341+Manikranthi@users.noreply.github.com>
2021-01-21 05:44:46 -0500

    Add files via upload

---
+++ day_1/division.py
@@ -0,0 +1,6 @@
+def art_op():
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+art_op()    
---
+++ day_1/loops.py
@@ -0,0 +1,6 @@
+def squ(n):
+    if(n>1):
+        for i in range(0,n):
+            print(i*i)
+squ(int(input()))            
+            
