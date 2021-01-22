72d28faaa6f5872b28f9723c502a19226acbc507
Manikranthi <77731341+Manikranthi@users.noreply.github.com>
2021-01-22 04:09:50 -0500

    Add files via upload

---
+++ day_3/find_angle_MBC.py
@@ -0,0 +1,5 @@
+import math
+a=int(input())
+b=int(input())
+c=math.atan(1.0*a/b)
+print (str(int(round(math.degrees(c))))+'°')
---
+++ day_3/polar_coordinates.py
@@ -0,0 +1,5 @@
+import cmath
+a=complex(input())
+b=cmath.polar(a)
+print(b[0])
+print(b[1])
